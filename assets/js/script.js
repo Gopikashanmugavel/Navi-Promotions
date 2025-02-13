06a127eaacccf962a4559131fc7fff4c70fcf05a
S.Gopika <138609412+Gopikashanmugavel@users.noreply.github.com>
2025-02-13 11:06:00 +0530

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -178,27 +178,31 @@
 
 
 	//Accordion Box
-	if($('.accordion-box').length){
-		$(".accordion-box").on('click', '.acc-btn', function() {
-			
+	if ($('.accordion-box').length) {
+		$(".accordion-box").on('click', '.acc-btn', function () {
+	
 			var outerBox = $(this).parents('.accordion-box');
 			var target = $(this).parents('.accordion');
-			
-			if($(this).hasClass('active')!==true){
+	
+			if ($(this).hasClass('active')) {
+				// If already active, remove active class and slide up content
+				$(this).removeClass('active');
+				target.removeClass('active-block');
+				$(this).next('.acc-content').slideUp(300);
+			} else {
+				// Close other open items
 				$(outerBox).find('.accordion .acc-btn').removeClass('active');
-			}
-			
-			if ($(this).next('.acc-content').is(':visible')){
-				return false;
-			}else{
-				$(this).addClass('active');
 				$(outerBox).children('.accordion').removeClass('active-block');
 				$(outerBox).find('.accordion').children('.acc-content').slideUp(300);
+	
+				// Open the clicked item
+				$(this).addClass('active');
 				target.addClass('active-block');
-				$(this).next('.acc-content').slideDown(300);	
+				$(this).next('.acc-content').slideDown(300);
 			}
-		});	
+		});
 	}
+	
 
 
 	// banner-carousel
